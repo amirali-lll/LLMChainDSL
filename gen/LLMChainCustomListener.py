4b2ab9a6ea0d890de08c54592bf156b7c4bc3ed8
amirali-lll <amirali.dst.lll@gmail.com>
2025-01-27 03:01:13 +0330

    override some methods to change their name

--- gen/LLMChainCustomListener.py
+++ gen/LLMChainCustomListener.py
@@ -6,7 +6,11 @@ from gen.LLMChainListener import LLMChainListener
 
 class LLMChainCustomListener(LLMChainListener):
     def __init__(self, rule_names):
-        self.overridden_rules = []
+        self.overridden_rules = [ 
+            'defineStatement' ,
+            'loadStatement',
+            'actionStatement',
+            'outputStatement' ]
         self.rule_names = rule_names
         self.ast = AST()
     
@@ -14,5 +18,17 @@ class LLMChainCustomListener(LLMChainListener):
         rule_name = self.rule_names[ctx.getRuleIndex()]
         if rule_name not in self.overridden_rules:
             make_ast_subtree(self.ast, ctx, rule_name)
-            
     
+        
+    def exitDefineStatement(self, ctx):
+        make_ast_subtree(self.ast, ctx, "define", keep_node=True)
+        
+    def exitLoadStatement(self, ctx):
+        make_ast_subtree(self.ast, ctx, "load", keep_node=True)
+        
+    def exitActionStatement(self, ctx):
+        make_ast_subtree(self.ast, ctx, "action", keep_node=True)
+        
+    def exitOutputStatement(self, ctx):
+        make_ast_subtree(self.ast, ctx, "output", keep_node=True)
+        
