e23185c8f98163bf5cec5f9983695569c9117875
amirali-lll <amirali.dst.lll@gmail.com>
2025-01-27 07:18:36 +0330

    Refactor SemanticAnalyzer to implement visit methods for analyze, summarize, expand, and refine statements; remove unused action checks.

--- semantic/semantic_analyzer.py
+++ semantic/semantic_analyzer.py
@@ -1,5 +1,4 @@
 from gen.LLMChainVisitor import LLMChainVisitor
-from .actions import checkAnalyzeOrSummarize, checkExpandOrRefine
 
 class SemanticAnalyzer(LLMChainVisitor):
     def __init__(self):
@@ -18,11 +17,72 @@ class SemanticAnalyzer(LLMChainVisitor):
         if input_name in self.symbol_table:
             raise Exception(f"Input '{input_name}' is already loaded.")
         self.symbol_table[input_name] = {"type": input_type}
-    
-# TODO: implement SemanticAnalyzer for 4 types of actions
-
+        
     
     def visitOutputStatement(self, ctx):
         output_name = ctx.name().getText()
         if output_name not in self.symbol_table:
             raise Exception(f"Output '{output_name}' is not defined.")
+
+    def visitAnalyzeStatement(self, ctx):
+        input_name = ctx.getChild(1).getText()
+        model_name = ctx.getChild(3).getText()
+        output_name = ctx.getChild(5).getText()
+        
+        if input_name not in self.symbol_table:
+            raise Exception(f"Input '{input_name}' is not defined.")
+        if model_name not in self.symbol_table:
+            raise Exception(f"Model '{model_name}' is not defined.")
+        if output_name in self.symbol_table:
+            raise Exception(f"Output '{output_name}' is already defined.")
+        
+        self.symbol_table[output_name] = {"type": "TEXT"}
+
+    def visitSummarizeStatement(self, ctx):
+        input_name = ctx.getChild(1).getText()
+        model_name = ctx.getChild(3).getText()
+        output_name = ctx.getChild(5).getText()
+        
+        if input_name not in self.symbol_table:
+            raise Exception(f"Input '{input_name}' is not defined.")
+        if model_name not in self.symbol_table:
+            raise Exception(f"Model '{model_name}' is not defined.")
+        if output_name in self.symbol_table:
+            raise Exception(f"Output '{output_name}' is already defined.")
+        
+        self.symbol_table[output_name] = {"type": "TEXT"}
+
+    def visitExpandStatement(self, ctx):
+        input_name = ctx.getChild(1).getText()
+        context_name = ctx.getChild(3).getText()
+        model_name = ctx.getChild(5).getText()
+        output_name = ctx.getChild(7).getText()
+        
+        if input_name not in self.symbol_table:
+            raise Exception(f"Input '{input_name}' is not defined.")
+        if context_name not in self.symbol_table:
+            raise Exception(f"Context '{context_name}' is not defined.")
+        if model_name not in self.symbol_table:
+            raise Exception(f"Model '{model_name}' is not defined.")
+        if output_name in self.symbol_table:
+            raise Exception(f"Output '{output_name}' is already defined.")
+        
+        self.symbol_table[output_name] = {"type": "TEXT"}
+        
+    def visitRefineStatement(self, ctx):
+        input_name = ctx.getChild(1).getText()
+        context_name = ctx.getChild(3).getText()
+        model_name = ctx.getChild(5).getText()
+        output_name = ctx.getChild(7).getText()
+        
+        if input_name not in self.symbol_table:
+            raise Exception(f"Input '{input_name}' is not defined.")
+        if context_name not in self.symbol_table:
+            raise Exception(f"Context '{context_name}' is not defined.")
+        if model_name not in self.symbol_table:
+            raise Exception(f"Model '{model_name}' is not defined.")
+        if output_name in self.symbol_table:
+            raise Exception(f"Output '{output_name}' is already defined.")
+        
+        self.symbol_table[output_name] = {"type": "TEXT"}
+        
